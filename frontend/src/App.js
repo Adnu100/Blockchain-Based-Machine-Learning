a56d26fcaea93beb76ee4a0b3134f8a455bdbaa0
Adnu100 <dhamangaonadnesh@gmail.com>
2021-03-11 18:44:11 +0530

    removed static css files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
-import header from "./components/Header";
-import footer from "./components/Footer";
 import StartProcess from "./components/StartProcess";
 import DownloadModel from "./components/DownloadModel";
 import HomePage from "./components/HomePage";
@@ -11,22 +14,21 @@ import "./static/gradientform.css";
 function App() {
   return (
     <div>
-    <Router>
-          <div className="App">
-            <Switch>
-              <Route exact path="/DownloadModel" component={DownloadModel} />
-              <Route exact path="/StartProcess" component={StartProcess} />
-              <Route exact path="/HomePage" component={HomePage} />
-              <Redirect from="/" to="/HomePage" />
-            </Switch>
-          </div>
-        </Router>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/DownloadModel" component={DownloadModel} />
+            <Route exact path="/StartProcess" component={StartProcess} />
+            <Route exact path="/HomePage" component={HomePage} />
+            <Redirect from="/" to="/HomePage" />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
 
 /*
   <FileInput></FileInput> 
@@ -57,3 +59,4 @@ export default App;
 
 >>>>>>> dad44a7c0a620fb796daf3e64787fbb909e21537
 */
+
